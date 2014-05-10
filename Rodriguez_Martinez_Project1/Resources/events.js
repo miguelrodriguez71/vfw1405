41b41282e57230020474262c181f2180db07b5d9
Miguel Rodriguez <miguelrodriguez@fullsail.edu>
2014-05-10 18:28:03 -0400

    Create events.js to copy all Events and Comments added.

--- Rodriguez_Martinez_Project1/Resources/events.js
+++ Rodriguez_Martinez_Project1/Resources/events.js
@@ -1,1 +1,24 @@
-previousLabel.addEventListener("click",nextTitle);
+// Click Previous Event
+var previousTitle = function() {
+	if (sportsIndex === 0) {
+		sportsIndex = 4;
+	} else {
+		sportsIndex = sportsIndex - 1;	
+	};
+	sportLabel.text = sports[sportsIndex];
+};
+
+
+// Click Next Event
+var nextTitle = function() {
+	if (sportsIndex === 4) {
+		sportsIndex = 0;
+	} else {
+		sportsIndex = sportsIndex + 1;		
+	};
+	sportLabel.text = sports[sportsIndex];
+};
+
+// Call Events for button
+previousView.addEventListener("click",previousTitle);
+nextView.addEventListener("click",nextTitle);
