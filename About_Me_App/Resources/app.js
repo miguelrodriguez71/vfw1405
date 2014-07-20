28f2a93bff0fceaf782c489ac0b1ebfaf6722941
Miguel Rodriguez <miguelrodriguez@fullsail.edu>
2014-07-19 22:39:54 -0400

    About me App complete. Ready to be Push

---
+++ About_Me_App/Resources/app.js
@@ -0,0 +1,20 @@
+// Rodriguez Miguel
+// Mobile Interfaces and Usability - 1407
+// About me App
+
+// Loading Data and Building Table
+
+
+
+Titanium.UI.setBackgroundColor('#000');
+
+// Create Main screen
+var mainScreen = Ti.UI.createWindow({
+	title: "About me App",
+	backgroundColor: "#408ede"
+	
+});
+
+var loadfile = require("json");
+
+mainScreen.open();
